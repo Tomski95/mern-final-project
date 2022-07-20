782e5f7597de1290722ec2513d58aedc6549139e
Tomski95 <tom_ski18@yahoo.com>
2022-07-20 13:59:12 +0200

    initial commit

---
+++ backend/public/build/precache-manifest.5bed59d6da58cdeb5107886861a5d115.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0279d0bf9b993d027d6659132c58a924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc47567ef23dde469fb",
+    "url": "/static/css/3.38453735.chunk.css"
+  },
+  {
+    "revision": "f6891787c4256a15c83a",
+    "url": "/static/css/4.ff4faa8e.chunk.css"
+  },
+  {
+    "revision": "303c485b6ac480eeeb4c",
+    "url": "/static/css/5.8cd69542.chunk.css"
+  },
+  {
+    "revision": "fbd20314397156331177",
+    "url": "/static/css/6.1326459c.chunk.css"
+  },
+  {
+    "revision": "a18167b93c307ebaa381",
+    "url": "/static/css/7.295134cd.chunk.css"
+  },
+  {
+    "revision": "99accc594be921b2f51f",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "af86c08cb959879187fa",
+    "url": "/static/js/2.47888315.chunk.js"
+  },
+  {
+    "revision": "8bc47567ef23dde469fb",
+    "url": "/static/js/3.2b2bcb85.chunk.js"
+  },
+  {
+    "revision": "f6891787c4256a15c83a",
+    "url": "/static/js/4.4c02d575.chunk.js"
+  },
+  {
+    "revision": "303c485b6ac480eeeb4c",
+    "url": "/static/js/5.12103971.chunk.js"
+  },
+  {
+    "revision": "fbd20314397156331177",
+    "url": "/static/js/6.9f56c28f.chunk.js"
+  },
+  {
+    "revision": "a18167b93c307ebaa381",
+    "url": "/static/js/7.2c779d85.chunk.js"
+  },
+  {
+    "revision": "99accc594be921b2f51f",
+    "url": "/static/js/main.3cef6a19.chunk.js"
+  },
+  {
+    "revision": "62c304a12f64c07c427c",
+    "url": "/static/js/runtime-main.0bb80160.js"
+  }
+]);
